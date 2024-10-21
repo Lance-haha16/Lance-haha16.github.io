811b270e3f1ea2ee8320a519e1c70f3e1baefe59
Lance-haha16 <3265221692cyz@gmail.com>
2024-10-21 23:47:53 +0800

    Update Tr1.py

--- Tr1.py
+++ Tr1.py
@@ -1,8 +1,17 @@
 from translate import Translator
 
-initial_language=input("initisl lsngusge:").capitalize()
-ultimate_language=input("ultimate language:").capitalize()
-translator=Translator(initial_language,ultimate_language)
-txt=input("请输入需要翻译的句子:")
-translation=translator.translate(txt)
-print(translation)
+# Get initial and ultimate languages
+initial_language = input("Initial language (e.g., 'en' for English): ")
+ultimate_language = input("Ultimate language (e.g., 'fr' for French): ")
+
+# Create a Translator object
+translator = Translator(from_lang=initial_language, to_lang=ultimate_language)
+
+# Input text to translate
+txt = input("请输入需要翻译的句子: ")  # You can keep this prompt in Chinese
+
+# Perform translation
+translation = translator.translate(txt)
+
+# Print the translation
+print("Translation:", translation)
